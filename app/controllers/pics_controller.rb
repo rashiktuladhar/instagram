b010d41fe39474858ba54fb05f5120956bd55892
Rashik <rashik.tuladhar@outlook.com>
2016-04-22 07:21:17 +0545

    some new methods added new and create and private method as pic_params added to keep the system DRY and corresponding new-view page added for as to create a form

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,7 +1,24 @@
 class PicsController < ApplicationController
 
-	
+
 	def index
 	end
+
+	def new
+		@pic = Pic.new
+	end
+
+	def create
+		@pic = Pic.new(pci_params)
+	end
+
+	
+	# private methods
+	private
+
+	def pic_params
+		params.require(:pic).permit(:title, :description)
+	end
+
 
 end
